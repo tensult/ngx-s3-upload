c5e76ae7ad88fb5b2415e3034c83c067cd40755c
parag poddar <paragpoddar01@gmail.com>
2018-04-06 16:03:06 +0530

    Modify configurations

--- src/app/auth/config.ts
+++ src/app/auth/config.ts
@@ -1,8 +1,8 @@
 export let cognito = {
   userPool: {
-    UserPoolId: 'us-east-1_o07i6UNpK',
-    ClientId: 'a3us4rrhlvcso6eid68c5ue91',
-    region: 'us-east-1'
+    UserPoolId: 'USER_POOL_ID',
+    ClientId: 'CLIENT_ID',
+    region: 'REGION'
   },
-  identityPoolId: 'us-east-1:cf1c1866-5331-409a-808e-dfa1925bf9fc'
+  identityPoolId: 'IDENTITY_POOL_ID'
 };
